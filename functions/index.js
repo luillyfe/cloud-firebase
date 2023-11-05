757324f8cc3d703314f9b0c79947a46866d61091
Fermin Blanco <luillyfe89@gmail.com>
2023-11-05 15:56:35 -0500

    features: adding function to handle json file imports

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const admin = require("firebase-admin");
+const { getStorage } = require("firebase-admin/storage");
+const logger = require("firebase-functions/logger");
+const { onObjectFinalized } = require("firebase-functions/v2/storage");
+
+const { v4: uuidv4 } = require("uuid");
+
+admin.initializeApp();
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+exports.importJSON = onObjectFinalized(async (event) => {
+  logger.info("processing file", { structuredData: true });
+  const db = admin.firestore();
+
+  const fileBucket = event.data.bucket; // Storage bucket containing the file.
+  const filePath = event.data.name;
+
+  const bucket = getStorage().bucket(fileBucket);
+
+  // Download file to memory, then convert to Json
+  const downloadResponse = await bucket.file(filePath).download();
+  const json = JSON.parse(downloadResponse.toString());
+
+  // Get batch client
+  const batch = db.batch();
+
+  // Perform updates
+  json.forEach((todo) => {
+    const ref = db.doc(`todos/${uuidv4()}`);
+    batch.set(ref, todo);
+  });
+
+  // Commit updates
+  await batch.commit();
+});
